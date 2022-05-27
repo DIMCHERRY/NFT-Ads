0a2eb9c5e2a64d4267c3708acf7b6a0a07e5828d
littlered <>
2022-05-28 04:54:58 +0800

    rinkeby

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -47,6 +47,11 @@ module.exports = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    rinkeby: {
+      url: process.env.Rinkeby_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     hardhat: {
     }
   },
